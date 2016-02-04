d0fcb293661a2dab907a7aae2648a35184e7344b
timgrossmann <t.grossmann1994@web.de>
2016-02-04 16:45:17 +0100

    Added first test and implementation for the input

--- StoreManagement/src/main/java/gui/Controller.java
+++ StoreManagement/src/main/java/gui/Controller.java
@@ -2,6 +2,7 @@ package gui;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.Scanner;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -9,27 +10,45 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
 
-public class Controller implements Initializable{
-	
+public class Controller implements Initializable {
+
 	@FXML
 	ListView<ItemBox> listView = new ListView<ItemBox>();
 	@FXML
 	Button testButton = new Button();
-	
+	@FXML
+	ToggleGroup addRemoveToggle;
+	@FXML
+	ToggleButton addButton;
+	@FXML
+	ToggleButton removeButton;
+
 	ObservableList<ItemBox> items = FXCollections.observableArrayList();
-	
 
 	public void initialize(URL location, ResourceBundle resources) {
 
+		Main.controller = this;
+
 		listView.setItems(items);
-		
+		addButton.setSelected(true);
+
 		testButton.setOnAction(event -> {
 			items.add(new ItemBox());
 		});
-		
+
 	}
-	
-	
+
+	public boolean addItem(String gtin) {
+		System.out.println("Add: "+ gtin);
+		return false;
+	}
+
+	public boolean removeItem(String gtin) {
+		System.out.println("Remove: "+ gtin);
+		return false;
+	}
 
 }
--- StoreManagement/src/main/java/gui/Main.java
+++ StoreManagement/src/main/java/gui/Main.java
@@ -7,15 +7,20 @@ import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-	
+
 	public static Stage primaryStage;
+	public static Scene primaryScene;
+	public static Controller controller;
+	private static String gtin = "";
+	private static Thread resetThread;
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 
 		AnchorPane root = FXMLLoader.load(getClass().getResource("GUI.fxml"));
 		Scene primaryScene = new Scene(root, root.getPrefWidth(), root.getPrefHeight());
-		
+		Main.primaryScene = primaryScene;
+
 		primaryStage.setScene(primaryScene);
 		primaryStage.setTitle("Storage Management System");
 		primaryStage.setMaxWidth(root.getPrefWidth());
@@ -23,12 +28,60 @@ public class Main extends Application {
 		primaryStage.setMinWidth(root.getPrefWidth());
 		primaryStage.setMinHeight(root.getPrefHeight());
 		Main.primaryStage = primaryStage;
+
 		primaryStage.show();
-		
+
+		primaryScene.setOnKeyPressed(event -> {
+
+			gtin += event.getText();
+
+		});
+
+		primaryStage.setOnCloseRequest(event -> {
+			killThread();
+		});
+
 	}
-	
+
+	private static void killThread() {
+		resetThread = null;
+	}
+
 	public static void main(String[] args) {
+
+		Runnable gtinReset = new Runnable() {
+
+			@Override
+			public void run() {
+				Thread thread = Thread.currentThread();
+				while (resetThread == thread) {
+					try {
+						Thread.sleep(3000);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+
+					if (!gtin.equals("")) {
+						
+						if(controller.addButton.isSelected()) {
+							controller.addItem(gtin);
+						} else if(controller.removeButton.isSelected()) {
+							controller.removeItem(gtin);
+						} else {
+							System.out.println("other");
+						}
+						
+						gtin = "";
+					}
+				}
+			}
+		};
+
+		resetThread = new Thread(gtinReset);
+		resetThread.start();
+
 		launch(args);
+
 	}
 
 }
