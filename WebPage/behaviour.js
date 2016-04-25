2d55fce496f92de0d106ab038f49e7ceb37aa7b1
timgrossmann <t.grossmann1994@web.de>
2016-04-25 20:19:32 +0200

    Finished the simple Webpage, you are now able to search the data on the localhost:3000/db if you start your json-server with --watch saveFile.json - This will now be set up on my apache and then the url will be added into my router as something easier than ip/webList.html, maybe just 'storage' or something like that

--- WebPage/behaviour.js
+++ WebPage/behaviour.js
@@ -1,17 +1,59 @@
+//Global var of the fetched ItemData
+var resultData;
+
+//upon pageLoad, all the Items are added to the table (sorted)
 $.getJSON("http://localhost:3000/db", function(data) {
-    
+
+    //Sorting the Items by their name
     data.sort(function(a, b) {
         return a["name"] > b["name"];
     });
+
+    //setting the global data var to the sorted Items
+    resultData = data;
     
-    for(var i in data) {
+    //Updating the table, allow all Item names by using "."
+    updateTable(/./, "name"); 
+});
+
+//listening on the input of the Text, reacting on change
+$("#searchField").on("input", handleEvent);
+
+//covering the change of the RadioButton
+$("input:radio[name='inlineRadioOptions']").on("change", handleEvent);
+
+//handle either the change of the text input or the radio buttons
+function handleEvent() {
+    //regex pattern with the value of the textfield, ignoring case
+    var regEx = new RegExp($("#searchField").val(), 'i');
+    
+    //getting the checked radioButtons
+    var selected = $("input:radio[name='inlineRadioOptions']:checked").val();
+    
+    //updating the table with the created RegEx and the selected radioButtons Value
+    updateTable(regEx, selected);
+}
+
+
+//Updates the table
+function updateTable(regEx, selected) {
+    
+    //setting the tables content to just the table headers
+    $("#tbl").html("<tr><th>Name</th><th>Amount</th><th>Categories</th></tr>");
+    
+    //filtering the fetched data for matching Items
+    var filtered = resultData.filter(function(a) {
+        //checking if the Items contain the string
+        return (regEx.test(a[selected]));
+    });
+    
+    //for every remaining Item, add it to the table with name, amount and categorie
+    for(var i in filtered) {
         
-        var name = data[i]['name'];
-        var amount = data[i]['amount'];
-        var categories = data[i]['categories'];
+        var name = filtered[i]['name'];
+        var amount = filtered[i]['amount'];
+        var categories = filtered[i]['categories'];
         
         $("#tbl").append('<tr><td>' + name + '</td><td>' + amount + '</td><td>' + categories + '</td></tr>');
-    }
-    
-    
-});
+    }  
+}
